6c6e36c2e5bfd65ad25c25ab6a86eb8b5a9a79aa
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-25 09:12:18 +0200

    qsort

--- prog-with-haskell/unit1/ex1.hs
+++ prog-with-haskell/unit1/ex1.hs
@@ -1,9 +1,14 @@
-doubleNum :: Int -> Int
-doubleNum num1 = num1 + num1
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+    where
+        smaller = [a | a <- xs, a >= x]
+        larger = [b | b <- xs, b < x]
 
 main :: IO ()
-
 main = do
-    let result = doubleNum (doubleNum 2)
+    print(sum [1, 6, 3])
 
-    print result
+    print(product [2, 3, 4])
+
+    print(qsort [5, 4, 10, 1])

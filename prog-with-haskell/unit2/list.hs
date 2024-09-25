6fc88f4c95b36509980a82935f275bc08ca07df4
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-25 11:52:25 +0200

    taking, droping elements from list

--- prog-with-haskell/unit2/list.hs
+++ prog-with-haskell/unit2/list.hs
@@ -1,6 +1,33 @@
+--- getting the first element of a list
 getFirst :: [Int] -> Int
 getFirst numList = do
     head numList
 
+--- removing the first item and taking other elements
+removeFirst :: [Int] -> [Int]
+removeFirst numList = do
+    tail numList
+
+--- Getting nth element from a list
+getNth :: [Int] -> Int -> Int
+getNth numList n
+    | n >= length numList = error "Out of range"
+    | otherwise = numList !! n
+
+--- Getting the first n element of a list
+getFirstNelement :: [Int] -> Int -> [Int]
+getFirstNelement numList n 
+    | n >= length numList = error "Out of range"
+    | otherwise = take n numList
+
+--- Removing the first n element of a list
+dropFirstNelement :: [Int] -> Int -> [Int]
+dropFirstNelement numList n 
+    | n >= length numList = error "Out of range"
+    | otherwise = drop n numList
+
 main :: IO ()
-main = print(getFirst [4, 1, 5])
+main = do
+    print(getFirst [4, 1, 5])
+    print(removeFirst[4, 2, 10, 43])
+    print(getNth [19, 30, 12] 1)
